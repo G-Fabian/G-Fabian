3d386dfa5d8bc5fb301b99bc4f13bf303afca80a
Fabian <fabiangtech@gmail.com>
2022-01-03 22:39:59 -0800

    First commit

---
+++ Java/Assignment_#1/src/assignment_/pkg1/Assignment_1.java
@@ -0,0 +1,16 @@
+package assignment_.pkg1;
+
+/**
+ *
+ * @author Fabian
+ */
+public class Assignment_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
